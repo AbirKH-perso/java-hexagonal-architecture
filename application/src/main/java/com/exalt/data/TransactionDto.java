d0253f352a885a1394545fbf73e5704eaecdd850
akhemir <abkhtoumi@gmail.com>
2022-11-01 20:35:40 +0100

    US3:see the history, add Transaction model

---
+++ application/src/main/java/com/exalt/data/TransactionDto.java
@@ -0,0 +1,29 @@
+package com.exalt.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionDto {
+
+  private long id;
+
+  private double amount;
+
+
+  private AccountDto account;
+
+
+  private String transactionInfo;
+
+  public TransactionDto(String info, double amount, AccountDto accounts) {
+    this.transactionInfo = info;
+    this.amount = amount;
+    this.account = accounts;
+  }
+}
