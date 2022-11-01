d66a9d4338ccb9d19e0cb0a066765b958e015dda
akhemir <abkhtoumi@gmail.com>
2022-11-01 19:08:39 +0100

    US1: make a deposit in my account, persistence adapter test RED

---
+++ adapteur-persistence/src/test/java/com/exalt/adapters/AccountDBAdapterTest.java
@@ -0,0 +1,37 @@
+package com.exalt.adapters;
+
+import com.exalt.data.AccountDto;
+import com.exalt.entities.Account;
+import com.exalt.ports.spi.AccountPersistencePort;
+import com.exalt.repository.AccountRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AccountDBAdapter.class})
+class AccountDBAdapterTest {
+    @Autowired
+    AccountPersistencePort accountPersistencePort;
+    @MockBean
+    private AccountRepository accountRepository;
+
+    @Test
+    void getAccountByNumberTest() {
+        AccountDto accountDto = accountPersistencePort.getAccountByNumber(1L);
+        Assertions.assertThat(accountDto).isNotNull();
+    }
+
+    @Test
+    void saveAccountTest() {
+        accountPersistencePort.saveAccount(new AccountDto());
+        Mockito.verify(accountRepository).save(any(Account.class));
+    }
+}
