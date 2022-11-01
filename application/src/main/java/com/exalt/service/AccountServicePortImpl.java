b49cc0ceced07d95f8153f714745c5e2d4f5ae20
akhemir <abkhtoumi@gmail.com>
2022-11-01 19:59:48 +0100

    EXTRA: create an account, application test GREEN

--- application/src/main/java/com/exalt/service/AccountServicePortImpl.java
+++ application/src/main/java/com/exalt/service/AccountServicePortImpl.java
@@ -4,7 +4,9 @@ import com.exalt.data.AccountDto;
 import com.exalt.ports.api.AccountServicePort;
 import com.exalt.ports.spi.AccountPersistencePort;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AccountServicePortImpl implements AccountServicePort {
     @Autowired
     private AccountPersistencePort accountPersistencePort;
