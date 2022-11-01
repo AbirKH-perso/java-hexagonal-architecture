462b8359a3d297d3e93f3865c4b9bdfb730cfaf4
akhemir <abkhtoumi@gmail.com>
2022-11-01 19:31:49 +0100

    US1: make a deposit in my account, api adapter test GREEN

--- adapteur-api/src/main/java/com/exalt/controller/AccountController.java
+++ adapteur-api/src/main/java/com/exalt/controller/AccountController.java
@@ -1,6 +1,8 @@
 package com.exalt.controller;
 
 import com.exalt.data.AccountDto;
+import com.exalt.ports.api.AccountDepositPort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private AccountDepositPort accountDepositPort;
+
     @PostMapping("/deposite")
     public ResponseEntity<AccountDto> deposite(@RequestParam double amount, @RequestParam long accountNumber){
-        return  new ResponseEntity<>( null, HttpStatus.NOT_FOUND);
+        return  new ResponseEntity<>( accountDepositPort.deposite(amount, accountNumber), HttpStatus.OK);
     }
 
 }
--- adapteur-api/src/test/java/com/exalt/controller/AccountControllerTest.java
+++ adapteur-api/src/test/java/com/exalt/controller/AccountControllerTest.java
@@ -1,9 +1,13 @@
 package com.exalt.controller;
 
+import com.exalt.data.AccountDto;
+import com.exalt.ports.api.AccountDepositPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,9 +22,12 @@ class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+    @MockBean
+    private AccountDepositPort accountDepositPort;
 
     @Test
     void depositeTest() throws Exception {
+        Mockito.when(accountDepositPort.deposite(100L,1L)).thenReturn(new AccountDto());
         mockMvc.perform(post("/account/deposite").param("amount","100").param("accountNumber","1"))
                 .andExpect(status().isOk());
     }
